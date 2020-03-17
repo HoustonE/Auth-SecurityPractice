71c57e2c70093c1e8a3ca499bbe1682c088634f8
Ethan Houston <48807099+HoustonE@users.noreply.github.com>
2020-03-17 12:21:51 -0400

    GoogleAuth2.0 upgrade, login with google, created project security credentials, updated login/register/logout to use passport with GoogleAuth2.0; added scocial-btns css

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const dotenv = require('dotenv');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -49,18 +50,43 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 //Passport
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//GoogleAuth2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({
+      googleId: profile.id
+    }, function(err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 //************ REST ************
 
@@ -171,6 +197,23 @@ app.route("/logout")
     req.logout();
     resp.redirect("/");
   });
+
+// -- GoogleAuth
+app.route("/auth/google")
+  .get(
+    passport.authenticate("google", {
+      scope: ["profile"]
+    })
+  );
+
+app.route("/auth/google/secrets")
+  .get(passport.authenticate('google', {
+    failureRedirect: '/login'
+  }),
+  function(req, resp){
+    resp.redirect('/secrets');
+  }
+);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
